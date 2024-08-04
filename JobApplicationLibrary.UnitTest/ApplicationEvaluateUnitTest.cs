5f3bb5a81380858753600ea011fb119ae56f54b4
Batuhan Bedir <batuhan.bedir133@gmail.com>
2024-08-04 13:54:34 +0300

    scenario expansion

--- JobApplicationLibrary.UnitTest/ApplicationEvaluateUnitTest.cs
+++ JobApplicationLibrary.UnitTest/ApplicationEvaluateUnitTest.cs
@@ -26,4 +26,47 @@ public class ApplicationEvaluateUnitTest
         //Assert
         Assert.AreEqual(appResult, ApplicationResult.AutoRejected);
     }
+
+    [Test]
+    public void Application_WithNoTechStack_TransferredToAutoRejected()
+    {
+        //Arrage
+        var evaluator = new ApplicationEvaluator();
+        var form = new JobApplication()
+        {
+            Applicant = new Applicant()
+            {
+                Age = 19
+            },
+            TechStackList = new List<string> { "" }
+        };
+
+        //Action
+        var appResult = evaluator.Evaluate(form);
+
+        //Assert
+        Assert.AreEqual(appResult, ApplicationResult.AutoRejected);
+    }
+
+    [Test]
+    public void Application_WithTechStackOver75P_TransferredToAutoAccepted()
+    {
+        //Arrage
+        var evaluator = new ApplicationEvaluator();
+        var form = new JobApplication()
+        {
+            Applicant = new Applicant()
+            {
+                Age = 20
+            },
+            TechStackList = new List<string> { "C#", "RabbitMQ", "Microservice", "Visual Studio" },
+            YearsOfExperience = 16
+        };
+
+        //Action
+        var appResult = evaluator.Evaluate(form);
+
+        //Assert
+        Assert.AreEqual(appResult, ApplicationResult.AutoAccepted);
+    }
 }
